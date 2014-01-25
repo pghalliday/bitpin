2de5af6fc644f9f79e40d5110a0d3b8a3224576d
Peter Halliday <pghalliday@gmail.com>
2014-01-25 22:29:16 +0100

    tidying

--- test/mylib.test.c
+++ test/mylib.test.c
@@ -8,18 +8,18 @@
 DESCRIBE(strcmp, "int strcmp ( const char * str1, const char * str2 )")
 
   IT( "returns 0 only when strings are equal" )
-    SHOULD_EQUAL( strcmp("hello", "hello"), 2)
-    SHOULD_NOT_EQUAL( strcmp("hello", "world"), 0)
+    SHOULD_EQUAL( strcmp("hello", "hello"), 0);
+    SHOULD_NOT_EQUAL( strcmp("hello", "world"), 0);
   END_IT
 
   IT( "returns a negative integer when str1 is less than str2"  )
-    SHOULD_BE_TRUE( strcmp("hello", "world") < 0 )
-    SHOULD_BE_TRUE( strcmp("0123", "1321431") < 0 )
+    SHOULD_BE_TRUE( strcmp("hello", "world") < 0 );
+    SHOULD_BE_TRUE( strcmp("0123", "1321431") < 0 );
   END_IT
 
   IT( "returns a positive integer if str1 is greater than str2"  )
-    SHOULD_BE_TRUE( strcmp("yellow", "world") < 0 )
-    SHOULD_BE_TRUE( strcmp("9", "789") > 0 )
+    SHOULD_BE_TRUE( strcmp("yellow", "world") > 0 );
+    SHOULD_BE_TRUE( strcmp("9", "789") > 0 );
   END_IT
 
 END_DESCRIBE
